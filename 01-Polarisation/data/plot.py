98c33bd1de22b5913db9725177aa843a08ab30b9
Unknown <amiculus.umut@hotmail.de>
2017-05-05 20:31:33 +0200

    01-working plot.py
    Former-commit-id: 85ac52c9fa71b715678510c4816481a9c82affd6

--- 01-Polarisation/data/plot.py
+++ 01-Polarisation/data/plot.py
@@ -9,16 +9,17 @@ if len(sys.argv)==1:
 	print('Specify a file, dimwit.')
 	sys.exit()
 
-elif len(sys.argv)==2:
-	print(sys.argv[1])
+elif len(sys.argv)>=2:
 	(deg, U) = np.loadtxt(sys.argv[1], unpack=True)
 
-N = 36
-theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
-width = np.pi / 4 * np.random.rand(N)
+N = 37
+theta = np.linspace(0.0, 2 * np.pi, N)
 
 ax = plt.subplot(111, projection='polar')
 ax.plot(theta, U, color='#ee8d18')
+
+for i in range(0,37):
+	print(i, theta[i], U[i])
 
 matplotlib.rc('text', usetex = True)
 params = {'text.latex.preamble' : ['\\usepackage{amsmath}', '\\usepackage{siunitx}', '\\sisetup{per-mode=fraction}', '\\sisetup{separate-uncertainty=true}']}
@@ -28,4 +29,4 @@ if len(sys.argv)==2:
 	plt.show()
 
 elif len(sys.argv)==3 and sys.argv[2]=='save':
-	plt.savefig('plots/' + sys.argv[1][:-3] + 'pdf', format='pdf')
+	plt.savefig('plots/' + sys.argv[1][7:-3] + 'pdf', format='pdf')
