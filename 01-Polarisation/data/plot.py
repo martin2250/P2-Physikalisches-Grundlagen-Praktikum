01-fixed rotation
Former-commit-id: 8d00292348c068fa209d1874d6e4c4cd7ad75331

01-Polarisation/data/plot.py
@@ -19,8 +19,7 @@ with open(sys.argv[1]) as file:
 		rotate = int(firstline[8:])
 		print(rotate)
 
-deg = np.roll(deg, rotate)
-U = np.roll(U, rotate)
+deg = deg + rotate
 
 theta = deg * np.pi / 180
 