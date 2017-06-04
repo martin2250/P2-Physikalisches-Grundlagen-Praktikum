b943cfe5668115d28946356490afc1e98342e341
Martin Pittermann <martinp2250@gmail.com>
2017-06-04 13:21:45 +0200

    04 - added A7

--- 04-Vakuum/data/6.py
+++ 04-Vakuum/data/6.py
@@ -7,6 +7,10 @@ import scipy.stats
 
 (P, Ui, Uh) = np.loadtxt('source/6.dat', unpack=True)
 
+matplotlib.rc('text', usetex = True)
+params = {'text.latex.preamble' : ['\\usepackage{amsmath}', '\\usepackage{siunitx}', '\\sisetup{per-mode=fraction}', '\\sisetup{separate-uncertainty=true}']}
+plt.rcParams.update(params)
+
 #plt.xkcd()
 
 ax = plt.gca()
@@ -15,11 +19,13 @@ ax.set_xscale('log')
 
 plt.grid(which='both')
 
-plt.plot(P, Ui, 'bo')
-plt.plot(P, Uh, 'ro')
+plt.plot(P, Ui, 'bo', label='$U_\\text{br}$')
+plt.plot(P, Uh, 'ro', label='$U_\\text{hold}$')
 
 plt.xlabel('Pressure')
 plt.ylabel('Voltage')
+
+plt.legend()
 
 if len(sys.argv) == 1:
 	plt.show()
