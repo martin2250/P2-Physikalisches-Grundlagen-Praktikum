6140eebbf18b06ad9752ef7d904f9f4fc9358d6e
Unknown <amiculus.umut@hotmail.de>
2017-06-06 18:22:22 +0200

    04-finished ex.2

--- 04-Vakuum/data/2.py
+++ 04-Vakuum/data/2.py
@@ -14,6 +14,13 @@ plt.rcParams.update(params)
 #plt.xkcd()
 
 ax = plt.gca()
+ax.set_yscale('log')
+
+a1, b1, r, p, stderr = scipy.stats.linregress(t, np.log(T1))
+a2, b2, r, p, stderr = scipy.stats.linregress(t, np.log(T2))
+
+plt.plot(t, np.exp(a1*t+b1), label=('fit of T1, $a_1=%.4f\\si{\\second}^{-1}$' % a1))
+plt.plot(t, np.exp(a2*t+b2), label=('fit of T2, $a_2=%.4f\\si{\\second}^{-1}$' % a2))
 
 plt.plot(t, T1, 'bo', label='$T_1$')
 plt.plot(t, T2, 'ro', label='$T_2$')
