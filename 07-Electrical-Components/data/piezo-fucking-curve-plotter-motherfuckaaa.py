cd967c549794ab4803785b22a6b11a14743c32d0
Martin Pittermann <martinp2250@gmail.com>
2017-07-04 18:45:06 +0200

    07 - fixed yo shit, motherfucker

--- 07-Electrical-Components/data/piezo-fucking-curve-plotter-motherfuckaaa.py
+++ 07-Electrical-Components/data/piezo-fucking-curve-plotter-motherfuckaaa.py
@@ -7,7 +7,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 import scipy.stats
 
-data  = np.load(sys.argv[2])['arr_0']
+if len(sys.argv) < 3:
+	print('motherfucker')
+	print('./piezo-shitty-file-name.py [input file] [y-axis unit] <output file>')
+	exit(1)
+
+data  = np.load(sys.argv[1])['arr_0']
 data = data.T
 
 matplotlib.rc('text', usetex = True)
@@ -18,9 +23,9 @@ plt.plot(data[0], data[1], color='red')
 plt.grid()
 
 plt.xlabel("time in \\si{\\milli\\second}")
-plt.ylabel("voltage in \\si{\\volt}")
+plt.ylabel("voltage in \\si{\\%s}"%sys.argv[2])
 
-if len(sys.argv) == 1:
+if len(sys.argv) == 3:
 	plt.show()
 else:
-    plt.savefig(sys.argv[1])
+    plt.savefig(sys.argv[3])
