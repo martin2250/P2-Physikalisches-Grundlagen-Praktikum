73051f23f34dc8bb19df6e287744ccfa88c057ca
Martin Pittermann <martinp2250@gmail.com>
2017-07-09 11:59:26 +0200

    08 - continued newton

--- 08-Interference/data/newton.py
+++ 08-Interference/data/newton.py
@@ -15,14 +15,14 @@ wavelength = D[0, 0] * 1e-9
 D = D[1:,:].T
 
 k = D[0]
-r = np.abs(D[2] - D[1]) * 1e-3
+r = np.abs(D[2] - D[1]) * 1e-3 / 2
 
 slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(k, r**2)
 
 R = slope/wavelength
 
 plt.ylabel('$r_k^2$ ($\\mathrm{mm^2}$)')
-plt.xlabel('$k$')
+plt.xlabel('Ring Order $k$')
 plt.grid()
 
 x = np.array([k[0], k[-1]])
@@ -31,7 +31,9 @@ plt.plot(x, (x*slope + intercept)*1e6, '-r')
 plt.plot(k, r**2 * 1e6, 'ob', label='$r_k$')
 
 if len(sys.argv) == 2:
-	plt.show()
 	print('curvature radius R =', R, 'm')
+	print('r^2_k/k = ', slope * 1e6, 'mm^2')
+	print('intercept = ', intercept, 'mm^2')
+	plt.show()
 else:
 	plt.savefig(sys.argv[2])
